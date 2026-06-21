5a84a1ed9555c5e0d2caa3e0ea6b769cd1a395a1
Carolina Bueno <petitearqstudy@gmail.com>
2026-06-21 11:33:10 -0300

    feat: add multiplication program that prompts for two numbers

---
+++ Exercicios Unimar/Lista 001/Exercicio 004.py
@@ -0,0 +1,9 @@
+'''4. Faça um Programa que peça dois números e imprima a multiplicação
+entre eles.
+'''
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = num1 * num2
+print(f"A multiplicação de {num1} e {num2} é: {resultado}")
